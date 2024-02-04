b6ebf21d24ac22d1f23ce8edae70b620fe3d737b
Mia <sumiret.6639@gmail.com>
2024-02-04 17:51:18 +0800

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
 	return {
 		// base: mode === "production" ? "/typescript-30/" : "/",
 		base: "/typescript-30/",
