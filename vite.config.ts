0446311b1fc9053af854077b9cc2021dd06bd926
Mia <sumiret.6639@gmail.com>
2024-02-04 17:50:49 +0800

    fix: base config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
 	return {
-		base: mode === "production" ? "/typescript-30/" : "/",
+		// base: mode === "production" ? "/typescript-30/" : "/",
+		base: "/typescript-30/",
 		plugins: [react()],
 	};
 });
