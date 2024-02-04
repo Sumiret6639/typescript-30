95a228f7cafa3ed60b1cc429adb8bbbbc78a5db7
Mia <sumiret.6639@gmail.com>
2024-02-04 17:58:11 +0800

    fix: react router config

--- src/main.tsx
+++ src/main.tsx
@@ -8,30 +8,33 @@ import Home from "./pages/Home.tsx";
 import Register from "./pages/Register.tsx";
 import Login from "./pages/Login.tsx";
 
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-		children: [
-			{
-				path: "/",
-				element: <Home />,
-			},
-			{
-				path: "/info",
-				element: <Info />,
-			},
-			{
-				path: "/register",
-				element: <Register />,
-			},
-			{
-				path: "/login",
-				element: <Login />,
-			},
-		],
-	},
-]);
+const router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			element: <App />,
+			children: [
+				{
+					path: "/",
+					element: <Home />,
+				},
+				{
+					path: "/info",
+					element: <Info />,
+				},
+				{
+					path: "/register",
+					element: <Register />,
+				},
+				{
+					path: "/login",
+					element: <Login />,
+				},
+			],
+		},
+	],
+	{ basename: "/typescript-30/" }
+);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
