a25c3fb8e298339e3ace3e63f5503c60e9e7920e
Mia <sumiret.6639@gmail.com>
2024-02-04 17:29:55 +0800

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-	// const env = loadEnv(mode, process.cwd(), "");
-	console.log(mode);
 	return {
 		base: mode === "production" ? "/typescript-30/" : "/",
 		plugins: [react()],
